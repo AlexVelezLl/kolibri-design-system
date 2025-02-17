9408b69035038841cfd0bab5d56ee3d84a58ea5b
Alex Velez <velezllaquealex@gmail.com>
2025-02-17 10:33:30 -0500

    Just another test

--- lib/buttons-and-links/KButtonGroup.vue
+++ lib/buttons-and-links/KButtonGroup.vue
@@ -11,6 +11,7 @@
     name: 'KButtonGroup',
     render(createElement) {
       var children = [];
+
       // Add an existence catch in case nothing is passed into the slots.
       (this.$slots.default || []).forEach(element => {
         if (element.tag) {
